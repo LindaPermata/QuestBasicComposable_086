9069a2c18588ea34cf23bca9a63a1ee4cd2ee35f
LindaPermata <permatalinda531@gmail.com>
2025-10-16 15:14:08 +0700

    Menambahkan tata letak Row dasar dengan komponen yang berjarak.

--- app/src/main/java/com/example/questbasiccomposable_086/TataLetak.kt
+++ app/src/main/java/com/example/questbasiccomposable_086/TataLetak.kt
@@ -24,7 +24,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun ItataLetakColumn(modifier: Modifier) {
+fun TataLetakColumn(modifier: Modifier) {
     Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
         Text(text = "Komponen1")
         Text(text = "Komponen2")
@@ -32,5 +32,19 @@ fun ItataLetakColumn(modifier: Modifier) {
         Text(text = "Komponen4")
     }
 }
+
+@Composable
+fun TataletakRow(modifier: Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
+
 
 
